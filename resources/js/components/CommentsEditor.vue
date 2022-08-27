81ece88c697cc1a66a970627e82ba4c8d636fcfd
0djentd <58089540+0djentd@users.noreply.github.com>
2022-08-27 09:35:37 +0300

    Добавил компонент "CommentsEditor".

---
+++ resources/js/components/CommentsEditor.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-card rounded="lg" elevation="2">
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="newComment.name"
+              :rules="nameRules"
+              label="Name"
+              dense
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="newComment.text"
+              :rules="textRules"
+              label="Text"
+              dense
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="12" md="4">
+            <date-picker
+              v-model="newComment.date"
+              type="datetime"
+            ></date-picker>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-btn @click.prevent="clear">Clear</v-btn>
+          <v-btn @click.prevent="post">Post</v-btn>
+        </v-row>
+      </v-container>
+    </v-form>
+  </v-card>
+</template>
+
+<script>
+import DatePicker from "vue2-datepicker";
+export default {
+  data() {
+    return {
+      newEmptyComment: {
+        text: "",
+        name: "",
+        date: "",
+      },
+      newComment: null,
+      nameRules: [(v) => !!v || "This field is required"],
+      textRules: [(v) => !!v || "This field is required"],
+      dateRules: [(v) => !!v || "This field is required"],
+    };
+  },
+  components: { DatePicker },
+  methods: {
+    clear() {
+        this.newComment = {...this.newEmptyComment};
+    },
+    post() {
+        this.$store.comments.commit("post", this.newComment);
+    },
+  },
+  mounted() {
+    this.clear();
+  }
+};
+</script>
