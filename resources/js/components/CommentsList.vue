59c9d87f414bb64f4ea41c390a6f3ee930c4c4b6
0djentd <58089540+0djentd@users.noreply.github.com>
2022-08-27 09:33:41 +0300

    Добавил компонент "CommentsList".

---
+++ resources/js/components/CommentsList.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-sheet rounded="lg" elevation="2">
+    <v-data-table
+      :loading="!comments"
+      :search="search"
+      :headers="headers"
+      :items="comments"
+      :items-per-page="3"
+      :footer-props="{ 'items-per-page-options': [3, 5, 10, 30, -1] }"
+      sort-by="id"
+    >
+      <!-- eslint-disable-next-line -->
+      <template v-slot:item.actions="{ item }">
+        <v-icon small @click="deleteComment(item)"> mdi-delete </v-icon>
+      </template>
+    </v-data-table>
+  </v-sheet>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      headers: [
+        { text: "ID", value: "id" },
+        { text: "Name", value: "name", sortable: false },
+        { text: "Text", value: "text", sortable: false },
+        { text: "Date", value: "date" },
+        { text: "Actions", value: "actions", sortable: false },
+      ],
+    };
+  },
+  computed: {
+    comments() {
+      return this.$store.state.comments.comments;
+    },
+  },
+  methods: {
+    deleteComment(comment) {
+      this.$store.commit("deleteComment", comment.id);
+    },
+  },
+  mounted() {
+    this.$store.comments.commit("getComments");
+  },
+};
+</script>
--- resources/js/views/app.vue
+++ resources/js/views/app.vue
@@ -1,18 +1,15 @@
 <template>
-    <div>
-        {{message}}
-    </div>
+    <v-main>
+        <CommentsList />
+        <CommentsEditor />
+    </v-main>
 </template>
+
 <script>
 const default_layout = "default";
-
-
+import CommentsList from "../components/CommentsList.vue"
+import CommentsEditor from "../components/CommentsEditor.vue"
 export default {
-    computed: {},
-    data() {
-        return {
-            message:'Hello World'
-        }
-    }
+  components: { CommentsList, CommentsEditor }
 };
 </script>
