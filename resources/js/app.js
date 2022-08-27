f248428def7fd2732aeaaabf077488e3e7974ec6
0djentd <58089540+0djentd@users.noreply.github.com>
2022-08-27 08:25:01 +0300

    Добавил Vuex и Vuetify.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import store from "./store"
+import vuetify from "./plugins/vuetify"
 
 //Main pages
 import App from './views/app.vue'
@@ -6,5 +8,7 @@ import App from './views/app.vue'
 
 const app = new Vue({
     el: '#app',
+    store,
+    vuetify,
     components: { App }
 });
